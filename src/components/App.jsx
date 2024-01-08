e87ee4f49fa46beb15104a8a6b35d8a887a75c42
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-01-08 20:21:08 +0200

    profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,17 @@
-import "./App.css";
+import './App.css';
+import { Profile } from './Profile/Profile';
+import userData from '../userData.json';
 
 const App = () => {
   return (
     <>
-      <p>Write your code here</p>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
     </>
   );
 };
