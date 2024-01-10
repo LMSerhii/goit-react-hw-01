944f8f5c8607977f9970107d630cd95edf2a6d30
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-01-10 15:51:32 +0200

    refactoring

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,27 +1,33 @@
 import css from './Profile.module.css';
 
-const Profile = props => {
+const Profile = ({
+  image,
+  name,
+  tag,
+  location,
+  stats: { followers, views, likes },
+}) => {
   return (
     <div className={css.profile}>
       <div className={css.description}>
-        <img src={props.image} alt="User avatar" className={css.avatar} />
-        <p className={css.name}>{props.name}</p>
-        <p className={css.tag}>@{props.tag}</p>
-        <p className={css.location}>{props.location}</p>
+        <img src={image} alt={name + ' avatar'} className={css.avatar} />
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
       <ul className={css.stats}>
         <li>
           <span className={css.label}>Followers</span>
-          <span className={css.value}>{props.stats.followers}</span>
+          <span className={css.value}>{followers}</span>
         </li>
         <li>
           <span className={css.label}>Views</span>
-          <span className={css.value}>{props.stats.views}</span>
+          <span className={css.value}>{views}</span>
         </li>
         <li>
           <span className={css.label}>Likes</span>
-          <span className={css.value}>{props.stats.likes}</span>
+          <span className={css.value}>{likes}</span>
         </li>
       </ul>
     </div>
